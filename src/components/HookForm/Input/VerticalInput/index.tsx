2dacd6f68d74e666fdc32ae332bb766c1d4c3445
LIONMAIL\brianjiang <brianjiang@liontravel.com>
2023-05-09 17:59:52 +0800

    adding form component

---
+++ src/components/HookForm/Input/VerticalInput/index.tsx
@@ -0,0 +1,27 @@
+import React, { forwardRef, memo } from "react";
+import type { InputHTMLAttributes } from "react";
+import { StyledLabel } from "./style";
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  description?: string;
+  errorMessage?: string;
+  hint?: string;
+}
+const TextInput = memo(
+  forwardRef<HTMLInputElement, InputProps>(function TextInput(
+    { label, description, errorMessage, hint, ...rest },
+    ref
+  ) {
+    return (
+      <StyledLabel>
+        <span className="title">{label}</span>
+        {description && <span className="description">{description}</span>}
+        <input className="input" ref={ref} {...rest}></input>
+        {errorMessage && <div className="error">{errorMessage}</div>}
+        {hint && <span className="hint">{hint}</span>}
+      </StyledLabel>
+    );
+  })
+);
+
+export default TextInput;
