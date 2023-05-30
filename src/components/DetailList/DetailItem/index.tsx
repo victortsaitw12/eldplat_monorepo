2607430c03cc0dd947f072adf2afd26a6dfe3438
PaulNI <paul86677889@gmail.com>
2023-05-30 17:49:39 +0800

    feat:新增組件們 II
    DetailList
    DetailItem
    VerticalDetail
    VerticalTextArea

---
+++ src/components/DetailList/DetailItem/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { I_listItem } from "@components/DetailList"
+import { BodySTY } from "./style";
+const DetailItem = ({ title, value }: I_listItem) => {
+    return (
+        <BodySTY className="detail_item">
+            <span>{title}</span>
+            <span>{value}</span>
+        </BodySTY>
+    )
+}
+
+export default DetailItem;
---
+++ src/components/DetailList/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BodySTY } from "./style";
+import DetailItem from "./DetailItem";
+
+export interface I_listItem {
+    title: React.ReactNode | string;
+    value: string
+}
+interface I_Props {
+    listArray: I_listItem[]
+}
+
+const DetailList = ({
+    listArray
+}: I_Props) => {
+    return (
+        <BodySTY className="detail_list">
+            {listArray && listArray.length > 0 ? listArray.map((child, i) => {
+                return (<DetailItem key={child.value + "-" + i} title={child.title} value={child.value} />)
+            }) :
+                <span>no data...</span>
+            }
+        </BodySTY>
+    )
+};
+export default DetailList
