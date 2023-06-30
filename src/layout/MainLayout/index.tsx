cb47d182acd7c5062fd29650831b452c581a6d69
PaulNI <paul86677889@gmail.com>
2023-06-30 11:16:07 +0800

    feat:sidebar menu要串接API

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -6,12 +6,35 @@ import Header from "./Header";
 import SideBar from "./SideBar";
 import { BodySTY, ContainerSTY } from "./style";
 import { fetchMenuData, MenuDataType } from "../../mock-data/side-bar/data";
+import { getSideMenuBackend } from "@services/siderbar/getSideMenuBackend";
+import { getSideMenuPersonal } from "@services/siderbar/getSideMenuPersonal";
 //
 const MainLayout: FC<{
   children: ReactNode;
   layoutProps: any;
 }> = ({ children, layoutProps }) => {
   const menuData: MenuDataType = fetchMenuData();
+  const [loading, setLoading] = React.useState(false);
+  const [menu, setMenu] = React.useState(null);
+
+  const fetch_menus = async () => {
+    setLoading(true);
+    try {
+      const res_menu = await getSideMenuBackend();
+      const res_personalmenu = await getSideMenuPersonal();
+      console.log("📃📃📃", res_menu);
+      console.log("😊😊😊", res_personalmenu);
+    } catch (e: any) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
+
+  React.useEffect(() => {
+    fetch_menus();
+  }, []);
+
+  console.log("main layout sidebar data");
   return (
     <BodySTY>
       <Head>
