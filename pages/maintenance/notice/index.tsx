36cf9ead73cafecbe4177d4e60b3ebca980641c0
Eddie <eddiewei@liontravel.com>
2023-07-07 17:12:58 +0800

    修改一些設計要求修改maintenance

--- pages/maintenance/notice/index.tsx
+++ pages/maintenance/notice/index.tsx
@@ -22,8 +22,8 @@ import AddMissionBtn from "@contents/maintenance/Notice/NoticeList/AddMissionBtn
 import MaintenanceCreateForm from "@contents/maintenance/MaintenanceCreateForm";
 //
 const mainFilterArray = [
-  { id: 1, label: "啟用", value: "1" },
-  { id: 2, label: "停用", value: "2" }
+  { id: 1, label: "通知", value: "1" },
+  { id: 2, label: "取消", value: "2" }
 ];
 //
 const Page: NextPageWithLayout<never> = () => {
