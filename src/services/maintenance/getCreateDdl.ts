c810dc3508a392e619c4f80b24e3b80ca88e47e2
Eddie <eddiewei@liontravel.com>
2023-07-25 18:17:03 +0800

    維修廠與項目DDL

--- src/services/maintenance/getCreateDdl.ts
+++ src/services/maintenance/getCreateDdl.ts
@@ -1,13 +1,14 @@
 import API_Path from "./apiPath";
 
 // 取ddl下拉式資料
-export const getCreateDdl = async (): Promise<any> => {
+export const getCreateDdl = async (no: string): Promise<any> => {
   const res = await fetch(API_Path["GetCreateDDL"], {
-    method: "GET",
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + process.env.NEXT_PUBLIC_ACCESS_TOKEN
-    }
+    },
+    body: JSON.stringify({ no })
   });
   return res.json();
 };
