c423bb7f566785604ebbc7a3f57bb4be3bdc8ade
jaxine <jaxinechang@liontravel.com>
2023-09-01 09:28:58 +0800

    change API url for developemnt

--- src/utils/createUrlPath.ts
+++ src/utils/createUrlPath.ts
@@ -1,7 +1,8 @@
 // testing server endpoint sample
 // http://uvehicle-api.eldplat.com/RPW/api/DownStream/GetGroupsData
 export function createUrlPath(microServiceName: string, apiName: string) {
-  // return `${process.env.NEXT_PUBLIC_PROD_ENDPOINT}/${microServiceName}/api/DownStream/${apiName}`;
+  return `${process.env.NEXT_PUBLIC_PROD_ENDPOINT}/${microServiceName}/api/DownStream/${apiName}`;
+  // FOR PRODUCTION AND DEVELOP (MAIN VS TESTING)
   return process.env.NODE_ENV === "production"
     ? `${process.env.NEXT_PUBLIC_PROD_ENDPOINT}/${microServiceName}/api/DownStream/${apiName}`
     : `${process.env.NEXT_PUBLIC_DEV_ENDPOINT}/${microServiceName}/${apiName}`;
