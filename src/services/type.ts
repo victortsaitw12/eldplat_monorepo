0d95f5c9739ef95896fb0f1a1d0e083265a4064c
jaxine <jaxinechang@liontravel.com>
2023-09-11 10:49:23 +0800

    fix: typescript error

--- src/services/type.ts
+++ src/services/type.ts
@@ -1,8 +1,8 @@
 export interface PageInfoType {
-  arrangement: "desc" | "asc"; //"desc"
-  orderby: string | null;
   page_Index: number; //1
   page_Size: number; //10
+  arrangement?: "desc" | "asc"; //"desc"
+  orderby?: string | null;
   last_Page?: number; //10
   total?: number; //5
 }
