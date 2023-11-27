fb5834f2c2487c1fb0e7291a44de0712424bb0ed
judyclee <judyclee@liontravel.com>
2023-11-27 22:44:14 +0800

    feature: add range datepicker

--- src/components/CustomDatePicker/index.tsx
+++ src/components/CustomDatePicker/index.tsx
@@ -5,14 +5,34 @@ import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 
 function CustomDatePicker(props: any) {
+  const { isRange = false, placeholder = "請輸入內容" } = props;
   const [startDate, setStartDate] = useState(new Date());
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [start, end] = dateRange;
+
+  const handleChange = (update: any) => {
+    setDateRange(update);
+  };
+  
   return (
     <BodySTY>
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => date && setStartDate(date)}
-        className="date-picker"
-      />
+      {isRange ? (
+        <DatePicker
+          selectsRange={true}
+          startDate={start}
+          endDate={end}
+          onChange={handleChange}
+          isClearable={true}
+        />
+      ) : (
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => date && setStartDate(date)}
+          className="date-picker"
+          placeholderText={placeholder}
+          locale="zh-TW"
+        />
+      )}
       <CalendarIcon className="icon" />
     </BodySTY>
   );
