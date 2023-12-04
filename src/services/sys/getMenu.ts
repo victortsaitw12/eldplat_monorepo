a1d734962bf134229bd3a655e0c414e9d311a077
jaxine <jaxinechang@liontravel.com>
2023-12-04 14:02:06 +0800

    use Dummy data for session

--- src/services/sys/getMenu.ts
+++ src/services/sys/getMenu.ts
@@ -25,7 +25,7 @@ export const getMenu = async (userId: string, creorgno: string) => {
 };
 
 // ------- MOCK DATA ------- //
-export const DUMMY_DATA = {
+const DUMMY_DATA = {
   StatusCode: "200",
   Message: "內部伺服器錯誤",
   DataList: [
@@ -115,4 +115,165 @@ export const DUMMY_DATA = {
   Result: true,
   ResultString: "BaseDomain",
   ResultInt: 0
+};
+
+export const DUMMY_MENU = {
+  userdefine: [],
+  defaultMenu: [
+    {
+      menu_name: "使用者管理",
+      sub_menu: [
+        {
+          module_no: "sys",
+          fg_no: "account",
+          func_no: "org",
+          func_name: "組織設定",
+          path: "/org"
+        },
+        {
+          module_no: "sys",
+          fg_no: "account",
+          func_no: "role",
+          func_name: "角色設定",
+          path: "/role"
+        },
+        {
+          module_no: "sys",
+          fg_no: "account",
+          func_no: "accountList",
+          func_name: "使用者列表",
+          path: "/account"
+        }
+      ]
+    },
+    {
+      menu_name: "車輛管理",
+      sub_menu: [
+        {
+          module_no: "bus",
+          fg_no: "bus",
+          func_no: "busList",
+          func_name: "車輛列表",
+          path: "/bus"
+        },
+        {
+          module_no: "bus",
+          fg_no: "bus",
+          func_no: "bus",
+          func_name: "車輛管理設定",
+          path: "/bus"
+        }
+      ]
+    },
+    {
+      menu_name: "維保管理",
+      sub_menu: [
+        {
+          module_no: "bus",
+          fg_no: "maintenance",
+          func_no: "notice",
+          func_name: "維保通知",
+          path: "/notice"
+        },
+        {
+          module_no: "bus",
+          fg_no: "maintenance",
+          func_no: "mission",
+          func_name: "維保任務列表",
+          path: "/mission"
+        },
+        {
+          module_no: "bus",
+          fg_no: "maintenance",
+          func_no: "maintenance",
+          func_name: "維保管理設定",
+          path: "/maintenance"
+        }
+      ]
+    },
+    {
+      menu_name: "駕駛管理",
+      sub_menu: [
+        {
+          module_no: "bus",
+          fg_no: "driver",
+          func_no: "driverList",
+          func_name: "駕駛列表",
+          path: "/driver"
+        },
+        {
+          module_no: "bus",
+          fg_no: "driver",
+          func_no: "driver",
+          func_name: "駕駛管理設定",
+          path: "/driver"
+        }
+      ]
+    },
+    {
+      menu_name: "任務管理",
+      sub_menu: [
+        {
+          module_no: "sys",
+          fg_no: "account",
+          func_no: "org",
+          func_name: "組織設定",
+          path: "/org"
+        },
+        {
+          module_no: "sys",
+          fg_no: "account",
+          func_no: "role",
+          func_name: "角色設定",
+          path: "/role"
+        },
+        {
+          module_no: "sys",
+          fg_no: "account",
+          func_no: "accountList",
+          func_name: "使用者列表",
+          path: "/account"
+        }
+      ]
+    },
+    {
+      menu_name: "訂單管理",
+      sub_menu: [
+        {
+          module_no: "bus",
+          fg_no: "order",
+          func_no: "orderList",
+          func_name: "訂單列表",
+          path: "/orderList"
+        }
+      ]
+    },
+    {
+      menu_name: "客戶管理",
+      sub_menu: []
+    },
+    {
+      menu_name: "供應商管理",
+      sub_menu: [
+        {
+          module_no: "bus",
+          fg_no: "vendor",
+          func_no: "car",
+          func_name: "外部車隊",
+          path: "/car"
+        },
+        {
+          module_no: "bus",
+          fg_no: "vendor",
+          func_no: "repairer",
+          func_name: "維修廠",
+          path: "/repairer"
+        }
+      ]
+    },
+    {
+      menu_name: "設定",
+      sub_menu: []
+    }
+  ]
 };
