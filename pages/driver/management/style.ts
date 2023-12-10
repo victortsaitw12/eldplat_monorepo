f7eb0d0b4b6a1e17575939b643f755049f50c01b
judyclee <judyclee@liontravel.com>
2023-12-10 12:33:56 +0800

    feature: add driver management

---
+++ pages/driver/management/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const BodySTY = styled.div`
+
+.wrapper{
+  padding: 0 20px;
+  display: flex;
+  gap: 20px;
+
+  .main-column {
+    flex: 1;
+  }
+}
+
+`;
