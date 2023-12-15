e2ea7ca0b6503d3e4617b23cb51b54f2f95baef6
jaxine <jaxinechang@liontravel.com>
2023-12-15 15:19:30 +0800

    fix: toggleCollapse

--- src/components/Table/CollapseTable/index.tsx
+++ src/components/Table/CollapseTable/index.tsx
@@ -9,8 +9,18 @@ import { DivSTY } from "./style";
 
 import Table, { I_Table } from "@components/Table/Table";
 import IconBtn from "@components/Button/IconBtn";
+import { head, set } from "lodash";
 
-function CollapseTable({ maxRow, ...props }: I_Props) {
+interface I_Props extends I_Table {
+  maxRow?: number;
+  hasControlAllBtns?: boolean;
+}
+
+function CollapseTable({
+  maxRow = 2,
+  hasControlAllBtns = true,
+  ...props
+}: I_Props) {
   const {
     titles,
     data,
@@ -21,11 +31,18 @@ function CollapseTable({ maxRow, ...props }: I_Props) {
     footerNode,
     className
   } = props;
-  const [collapse, setCollapse] = React.useState<number[]>([2]);
 
-  const toggleCollapse = (e: any) => {
+  const [collapse, setCollapse] = React.useState<number[]>([]);
+  const [isAllOpen, setIsAllOpen] = React.useState(false);
+  const [isAllFold, setIsAllFold] = React.useState(false);
+
+  console.log("🍅 collapse", collapse);
+
+  //------ functions ------//
+  const toggleCollapse = ({ e, i }: { e: any; i: number }) => {
     e.stopPropagation();
-    const index = e.target.value;
+    console.log("🍅 i", i);
+    const index = i + 1;
     if (collapse.includes(index)) {
       setCollapse(collapse.filter((item) => item !== index));
     } else {
@@ -33,26 +50,61 @@ function CollapseTable({ maxRow, ...props }: I_Props) {
     }
   };
 
+  const handleFoldAll = () => {
+    console.log("foldAll");
+  };
+  const handleOpenAll = () => {
+    console.log("openAll");
+  };
+
+  // ------ effect ------ //
+  React.useEffect(() => {
+    const getTotalTableRows = () => (data ? data.length : 0);
+    const totalTableRows = getTotalTableRows();
+    const totalCollapseRowsArr = Array.from(
+      { length: totalTableRows },
+      (_, i) => i
+    );
+    setCollapse(totalCollapseRowsArr.slice(1, totalTableRows + 1));
+  }, [data, isAllFold]);
+
+  React.useEffect(() => {
+    if (isAllOpen) {
+      setCollapse([]);
+    }
+  }, [isAllOpen]);
+
+  // ------- render ------- //
   const dataCollapse = data?.map((item, i) => {
     return {
       checkbox: (
         <IconButton
-          icon={collapse.includes(i) ? ChevronDownIcon : ChevronRightIcon}
+          icon={collapse.includes(i + 1) ? ChevronRightIcon : ChevronDownIcon}
           value={i}
-          onClick={toggleCollapse}
+          onClick={(e: any) => toggleCollapse({ e, i })}
         />
       ),
       ...item
     };
   });
 
+  const defaultHeadNode = (
+    <div className="headNode">
+      <div className="btns">
+        <Button onClick={handleFoldAll}>全部收合</Button>
+        <Button onClick={handleOpenAll}>全部展開</Button>
+      </div>
+      {headNode ? headNode : null}
+    </div>
+  );
+
   return (
-    <DivSTY className="collapseTable" collapse={collapse}>
+    <DivSTY className="collapseTable" collapse={collapse} maxRow={maxRow}>
       <Table
         titles={titles}
         data={dataCollapse}
         onView={onView}
-        headNode={headNode}
+        headNode={defaultHeadNode}
         onCheck={toggleCollapse}
       />
     </DivSTY>
@@ -60,7 +112,3 @@ function CollapseTable({ maxRow, ...props }: I_Props) {
 }
 
 export default CollapseTable;
-
-interface I_Props extends I_Table {
-  maxRow?: number;
-}
--- src/components/Table/CollapseTable/style.ts
+++ src/components/Table/CollapseTable/style.ts
@@ -1,11 +1,31 @@
 import styled from "styled-components";
 
-export const DivSTY = styled.div<{ collapse: number[] }>`
+const LINE_HEIGHT = 16;
+const getRowHeight = (maxRow: number) => {
+  return LINE_HEIGHT * maxRow;
+};
+
+export const DivSTY = styled.div<{ collapse: number[]; maxRow: number }>`
+  header {
+    padding: 0;
+  }
+  button {
+    z-index: 1;
+  }
   td > div {
     max-height: fit-content;
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
+    &::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 2rem;
+      width: 100%;
+      background: linear-gradient(to bottom, transparent, red);
+    }
   }
   tbody {
     tr {
@@ -16,8 +36,8 @@ export const DivSTY = styled.div<{ collapse: number[] }>`
   ${(props) =>
     props.collapse?.map((item) => {
       return `tr:nth-child(${item}) {
-        td>div{
-            max-height: 32px;
+        td > div{
+            max-height: ${getRowHeight(props.maxRow)}px;
             overflow: hidden;
             text-overflow: ellipsis;
             white-space: nowrap;
