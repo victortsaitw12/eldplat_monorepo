7c508608748f629f9f6db229a3a49f8bb14dc67e
gabonbon <judy860528@gmail.com>
2023-12-19 17:53:48 +0800

    feature: revise ButtonSet and ControlBar style

--- src/components/ButtonSet/style.ts
+++ src/components/ButtonSet/style.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const DivSTY = styled.div`
   display: flex;
-  padding: 12px 20px;
   justify-content: flex-end;
   align-items: center;
   gap: 10px;
