bfbd8fff096105542b5f9c4249f249a5225fe79a
gabonbon <judy860528@gmail.com>
2023-12-19 17:46:31 +0800

    fix: create button router

--- pages/bus/index.tsx
+++ pages/bus/index.tsx
@@ -75,6 +75,7 @@ const Page: NextPageWithLayout<never> = () => {
 
   const createBusHandler = () => {
     console.log("createBusHandler");
+    router.push("/bus/detail/create");
   };
 
   const controlButton = (
