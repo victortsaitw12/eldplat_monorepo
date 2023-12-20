222159eac8f7ea697f2a4d27fdb674248b583d4d
victortsai <victortsai@liontravel.com>
2023-12-20 14:47:52 +0800

    change progresslist color

--- pages/orders/detail/[id].tsx
+++ pages/orders/detail/[id].tsx
@@ -19,6 +19,7 @@ import { getCreateBusOptions } from "@services/bus/getCreateBusOptions";
 import DataOverview from "@components/DataOverview";
 import ControlBar from "@components/ControlBar";
 import ButtonSet from "@components/ButtonSet";
+import ProgressList from "@components/ProgressList";
 
 const mainTabsArray = [ 
   { id: 1, label: "訂單內容", value: "1" },
@@ -55,6 +56,15 @@ const Page: NextPageWithLayout<
     <BodySTY>
       <ControlBar hasShadow>
         {/* <DataOverview data={busDefaultData?.info} hasImage={false} /> */}
+        <ProgressList  dataLists={[
+                {label: "詢價中", date: "11/01 10:00", status: "ok"},
+                {label: "已報價", date: "11/01 13:00", status: "ok"},
+                {label: "接受報價", date: "11/01 15:30", status: "ok"},
+                {label: "訂單成立", date: "", status: "pending"},
+                {label: "建立任務", date: "", status: "pending"},
+                {label: "已付尾款", date: "", status: "pending"},
+                {label: "結案", date: "", status: "pending"},
+        ]} />
         <ButtonSet
           isEdit={isEdit}
           primaryDisable={false}
@@ -62,7 +72,7 @@ const Page: NextPageWithLayout<
           secondaryBtnOnClick={isEdit ? handleView : handleReturn}
           primaryBtnText={isEdit ? "儲存" : "編輯"}
           primaryBtnOnClick={isEdit ? handleView : handleEdit}
-          thirdBtnText={isEdit ? "儲存" : "送出報價"}
+          thirdBtnText={isEdit ? "" : "送出報價"}
           thirdBtnOnClick={isEdit ? handleView : handleEdit}
         />
       </ControlBar>
--- src/components/ProgressList/style.ts
+++ src/components/ProgressList/style.ts
@@ -15,7 +15,11 @@ export const ItemSTY = styled.div<{
   z-index: 2;
   .item-label {
     color: ${({ theme, status, color }) =>
-      status === "error" ? theme.color.R400 : color || theme.color.B400};
+      status === "error" 
+      ? theme.color.R400 
+      : status !== "ok" 
+      ? theme.color.N50 
+      : color || theme.color.N300};
     font-weight: 600;
   }
   .item-icon {
@@ -29,15 +33,15 @@ export const ItemSTY = styled.div<{
       status === "error"
         ? theme.color.R400
         : status !== "ok"
-        ? theme.color.N0
-        : color || theme.color.B400};
+        ? theme.color.N50
+        : color || theme.color.N300};
     color: ${({ theme }) => theme.color.N0};
     border: ${({ theme, status, color }) =>
-      (status === "pending" && `2px solid ${color || theme.color.B400}`) ||
+      (status === "pending" && `2px solid ${color || theme.color.N50}`) ||
       (status === "disabled" && `2px solid ${theme.color.N300}`)};
   }
   .item-date {
-    color: ${({ theme }) => theme.color.N500};
+    color: ${({ theme }) => theme.color.N300};
     font-size: 12px;
   }
 `;
@@ -56,7 +60,7 @@ export const LineSTY = styled.div<{ color?: string }>`
   position: absolute;
   top: 50%;
   left: 40px;
-  border: 1px solid ${({ theme, color }) => color || theme.color.B400};
+  border: 1px solid ${({ theme, color }) => color || theme.color.N50};
   width: calc(100% - 80px);
 `;
 export const GrayLineSTY = styled.div<{ grayWidth?: number }>`
